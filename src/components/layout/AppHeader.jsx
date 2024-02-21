b3e0bfbd186f2c8ee188c36888f50bf5ce4d5c22
Mikhail Elanov <Elanov1991@Gmail.com>
2024-02-21 10:07:11 +0700

    Add components - AppHeader, AppContent, AppSider

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,1 +1,14 @@
-export default function AppHeader() {}
+import { Layout } from "antd";
+
+const headerStyle = {
+  textAlign: "center",
+  color: "#fff",
+  height: 60,
+  paddingInline: 48,
+  lineHeight: "64px",
+  backgroundColor: "#4096ff",
+};
+
+export default function AppHeader() {
+  return <Layout.Header style={headerStyle}>Header</Layout.Header>;
+}
