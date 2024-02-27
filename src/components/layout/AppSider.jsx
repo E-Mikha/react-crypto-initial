a090a28c8a4567257ef273b6c5eb77bb74e5b1c5
Mikhail Elanov <Elanov1991@Gmail.com>
2024-02-27 11:47:20 +0700

    Add all informations to Card on list

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -2,22 +2,11 @@ import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
 import { Layout, Card, Statistic, List, Typography, Spin } from "antd";
 import { useEffect, useState } from "react";
 import { fakeFetchCrypto, fetchAssets } from "../../api";
+import { percentDeference } from "../../utils";
 
 const siderStyle = {
   padding: "1rem",
 };
-
-const data = [
-  "Racing car sprays burning fuel into crowd.",
-  "Japanese princess to wed commoner.",
-  "Australian walks 100km after outback crash.",
-  "Man charged over missing wedding girl.",
-  "Los Angeles battles huge wildfires.",
-];
-
-function percentDeference(a, b) {
-  return 100 * Math.abs((a - b) / ((a + b) / 2));
-}
 
 export default function AppSider() {
   const [loading, setLoading] = useState(false);
@@ -54,40 +43,34 @@ export default function AppSider() {
 
   return (
     <Layout.Sider width="25%" style={siderStyle}>
-      <Card style={{ marginBottom: "1rem" }}>
-        <Statistic
-          title="Active"
-          value={11.28}
-          precision={2}
-          valueStyle={{
-            color: "#3f8600",
-          }}
-          prefix={<ArrowUpOutlined />}
-          suffix="%"
-        />
-        <List
-          size="small"
-          dataSource={data}
-          renderItem={(item) => (
-            <List.Item>
-              <Typography.Text mark>[ITEM]</Typography.Text> {item}
-            </List.Item>
-          )}
-        />
-      </Card>
-
-      <Card>
-        <Statistic
-          title="Idle"
-          value={9.3}
-          precision={2}
-          valueStyle={{
-            color: "#cf1322",
-          }}
-          prefix={<ArrowDownOutlined />}
-          suffix="%"
-        />
-      </Card>
+      {assets.map((asset) => (
+        <Card key={asset.id} style={{ marginBottom: "1rem" }}>
+          <Statistic
+            title={asset.id}
+            value={asset.totalAmount}
+            precision={2}
+            valueStyle={{
+              color: asset.grow ? "#3f8600" : "#cf1322",
+            }}
+            prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+            suffix="$"
+          />
+          <List
+            size="small"
+            dataSource={[
+              { title: "Total Profit", value: asset.totalProfit },
+              { title: "Asset Amount", value: asset.amount },
+              { title: "Difference", value: asset.growPercent },
+            ]}
+            renderItem={(item) => (
+              <List.Item>
+                <span>{item.title}</span>
+                <span>{item.value}</span>
+              </List.Item>
+            )}
+          />
+        </Card>
+      ))}
     </Layout.Sider>
   );
 }
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export function percentDeference(a, b) {
+  return 100 * Math.abs((a - b) / ((a + b) / 2));
+}
