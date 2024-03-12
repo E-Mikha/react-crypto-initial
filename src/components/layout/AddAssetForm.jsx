3d35c18ac2a5890b3549eab41ef1f8c7c3d410c6
Mikhail Elanov <Elanov1991@Gmail.com>
2024-03-12 16:50:17 +0700

    Add Form for coins in Add Assets

--- src/components/layout/AddAssetForm.jsx
+++ src/components/layout/AddAssetForm.jsx
@@ -1,4 +1,13 @@
-import { Select, Space, Typography } from "antd";
+import {
+  Select,
+  Space,
+  Typography,
+  Flex,
+  Divider,
+  Form,
+  InputNumber,
+  Button,
+} from "antd";
 import { useState } from "react";
 import { useCrypto } from "../../context/crypto-context";
 
@@ -31,11 +40,61 @@ export default function AddAssetForm() {
     );
   }
 
+  function onFinish(value) {
+    console.log("finish", values);
+  }
+
   return (
-    <form>
-      <Typography.Title level={2} style={{ margin: 0 }}>
-        {coin.name}
-      </Typography.Title>
-    </form>
+    <Form
+      name="basic"
+      labelCol={{
+        span: 4,
+      }}
+      wrapperCol={{
+        span: 10,
+      }}
+      style={{
+        maxWidth: 600,
+      }}
+      initialValues={{}}
+      onFinish={onFinish}
+    >
+      <Flex align="center">
+        <img
+          src={coin.icon}
+          alt={coin.name}
+          style={{ width: 40, marginRight: 10 }}
+        />
+        <Typography.Title level={2} style={{ margin: 0 }}>
+          {coin.name}
+        </Typography.Title>
+      </Flex>
+      <Divider />
+
+      <Form.Item
+        label="Amount"
+        name="amount"
+        rules={[
+          {
+            required: true,
+            type: "number",
+            min: 0,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <InputNumber />
+      </Form.Item>
+
+      <Form.Item label="Price" name="price">
+        <InputNumber />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Add Asset
+        </Button>
+      </Form.Item>
+    </Form>
   );
 }
